c7dc9e5c37bdf07b7dc5a40a7bd8beb38f1c5f92
anacsr008 <ana08senai@gmail.com>
2025-12-04 15:54:16 -0300

    quarto commit

---
+++ resources/views/Comofunciona.blade.php
@@ -0,0 +1,33 @@
+\<!DOCTYPE html>
+<html lang="PT">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Submissão - Como funciona</title>
+    <link rel="stylesheet" href="/css/Comofunciona.css">
+</head>
+<body>
+ 
+    <div class="container">
+        
+      <h1 class="title">Como funciona</h1>
+      
+      <p>
+        Capitão Finny, o pequeno submarino azul, vive em um recife de coral. Todos os dias, as correntes do oceano trazem muitas moedas de ouro que caem no fundo do mar.
+      </p>
+      <p>
+        O problema é que também trazem muitos obstáculos que podem machucar Finny.
+      </p>
+      <p>
+        A missão de Finny é simples, mas perigosa: coletar no máximo de moedas de ouro que puder para manter o recife rico e bonito, enquanto desvia de tudo o que é perigoso para garantir que ele possa voltar para casa em segurança.
+      </p>
+
+        <div class="starfish top-left"></div>
+        <div class="starfish top-right-corner"></div>
+
+        <a href="#" class="copyright"> &lt; Voltar à página principal </a>
+       
+    </div>
+</body>
+</html>
--- resources/views/tela-inicial-jogo.blade.php
+++ resources/views/tela-inicial-jogo.blade.php
@@ -1,26 +1,33 @@
-\<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="PT">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> A lenda de Orion </title>
+    <title> Submissão - Página Inicial </title>
     <link rel="stylesheet" href="/css/tela-inicial.css">
 </head>
 <body>
-    <div class="menu-container">
-        <h1 class="game-title">A Lenda de Orion</h1>
-        
-        <p class="tagline">O destino do reino está em suas mãos.</p>
+    <div class="container">
+        <img src="imagens/flor01.png" alt="Flor canto superior esquerdo"class="graphic flor01">
+        <img src="imagens/flor02.png" alt="Flor meio direito"class="graphic flor02">
+        <img src="imagens/arraia.png" alt="Desenho de uma Arraia" class="graphic arraia">
+        <img src="imagens/água-viva.png" alt="Desenho de uma Água-Viva" class="graphic agua-viva">
+        <img src="imagens/Corais Azuis e Plantas Subaquáticas.png" alt="Desenho de Corais e Algas" class="graphic corais">
+        <img src="imagens/Cardume de Peixes Azuis.png" alt="Desenho de um cardume de peixes" class="graphic peixes">
+        <div class="starfish top-left"></div>
+        <div class="starfish top-right-corner"></div>
 
-        <div class="menu-buttons">
-            <button class="menu-btn start-btn">Iniciar Jogo</button>
-            <button class="menu-btn">Configurações</button>
-            <button class="menu-btn">Saiba Mais</button>
-            <button class="menu-btn exit-btn">Sair</button>
-        </div>
+        <h1 class="title">Sub Missão</h1>
         
-        <p class="version-info">Versão 1.0.0 - Original Ana</p>
+        <div class="menu">
+            <button class="menu-button">Iniciar jogo</button>
+            <button class="menu-button">Como funciona</button>
+            <button class="menu-button">Sair</button>
+        </div>
+
+        <p class="copyright"> Ana Clara Rosa | Senai | 2025</p>
+       
     </div>
 </body>
 </html>
